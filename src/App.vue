34f3328fd44bf37f55fa0d5883695a4376256f1b
ianfletcher55 <ian.fletcher55@gmail.com>
2020-07-13 10:13:31 -0500

    adds HTML theme, includes a few kinks

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/movies">All Movies</router-link> |
-      <router-link to="/movies/new">New Movie</router-link> |      
-      <span v-if="isLoggedIn()">
-        <router-link to="/logout">Logout</router-link>
-      </span>
-      <span v-else>
-        <router-link to="/signup">Signup</router-link> |
-        <router-link to="/login">Login</router-link>
-      </span>
+
+    <div id="navigation">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/movies">All Movies</router-link></li>
+        <li><router-link to="/movies/new">New Movie</router-link></li>
+        <span v-if="isLoggedIn()">
+          <li><router-link to="/logout">Logout</router-link></li>
+        </span>
+        <span v-else>
+          <li><router-link to="/signup">Signup</router-link></li>
+          <li><router-link to="/login">Login</router-link></li>
+        </span>
+      </ul>
     </div>
+    
     <router-view/>
+
+    <div id="footer">
+    <p class="lf">Copyright &copy; 2010 <a href="#">SiteName</a> - All Rights Reserved</p>
+    <p class="rf">Design by <a href="http://chocotemplates.com/">ChocoTemplates.com</a></p>
+    <div style="clear:both;"></div>
+
   </div>
+
+  </div>
+  
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
 
 <script>
